3dd63f833185c5ac02efb8e5235b3b2d403538c0
jessabean <jessica.avison@gmail.com>
2014-04-19 14:34:26 -0400

    Stub in push to array function

--- js/application.js
+++ js/application.js
@@ -1,3 +1,22 @@
 $( document ).ready(function() {
 
+  function printIndustries() {
+    var selectedIndustries = [];
+    // var input = $('input[type="checkbox"]:checked');
+
+    // $(input).each(function() {
+    //   // selectedIndustries.push($(this).val());
+    //     alert("super");
+    // });
+
+     $('input[type="checkbox"]:checked').each(function(){
+        selectedIndustries.push($(this).val());
+      });
+    console.log(selectedIndustries); 
+  }
+
+  $('button').click(function() {
+    printIndustries();
+  })
+    
 });
