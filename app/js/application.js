cb81c69d61efc7aceb06397f0522a14f8332d390
jessabean <jessica.avison@gmail.com>
2014-05-06 10:02:37 -0400

    AJAX submission

--- app/js/application.js
+++ app/js/application.js
@@ -15,15 +15,19 @@ $('input[type=checkbox]').click(function() {
 })
 
 $('button').click(function() {
-  var loadUrl = './confirm.html #main > *';
-
   if(selectedIndustries.length < 1) {
     alert("Please choose an industry.");
   } else if(selectedIndustries.length > 4) {
     alert("You can only select 4 industries!");
   } else {
-    $('#main').load(loadUrl, function() {
-      printIndustries(selectedIndustries);
+    $.ajax({
+      type: "POST",
+      url: "/response", // tell server what url you're trying to hitx
+      data: {industries: selectedIndustries}, // JSON key/value
+      dataType: 'html', // what you're expecting back from server
+      success: function(data) {
+        $('#main').html(data);
+      }
     });
   }
 });
