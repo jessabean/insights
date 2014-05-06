68649cfa20479813022b79d55543641384b75f0a
jessabean <jessica.avison@gmail.com>
2014-05-06 10:02:06 -0400

    Use ERB for response page

--- insights.rb
+++ insights.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
+set :views, settings.root + '/app/views'
 
 get '/' do
   File.read(File.join('public', 'index.html'))
+end
+
+post '/response' do
+  @industries = params[:industries]
+  erb :confirm
 end
